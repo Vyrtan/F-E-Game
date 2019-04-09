84541a61c8f2c0004a14f8df4b17df05fc3dff38
Vyrtan <jonas_langenberg@web.de>
2019-04-09 20:33:03 +0200

    database_test_working

---
+++ F-E-G_Jonas/Proto_Jonas/Assets/scripts/Changetext.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Changetext : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Aenderetext (string text)
+    {
+        if (GetComponent<UnityEngine.UI.Text>().text == "Start")
+        {
+            GetComponent<UnityEngine.UI.Text>().text = "Ende";
+        }
+        else { 
+        GetComponent<UnityEngine.UI.Text>().text = text;
+        }
+    }
+}
---
+++ F-E-G_Jonas/Proto_Jonas/Assets/scripts/sqlliteTest.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Data;
+using Mono.Data.Sqlite;
+using System.IO;
+
+public class sqlliteTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        //Connect to DB
+        string connection = "URI = file:" + Application.persistentDataPath + "/My_Database";
+        Debug.Log(connection);
+        IDbConnection dbcon = new SqliteConnection(connection);
+        dbcon.Open();
+
+        // Create table
+        IDbCommand dbcmd;
+        IDataReader reader;
+        dbcmd = dbcon.CreateCommand();
+        string q_createTable = "CREATE TABLE IF NOT EXISTS my_table (id INTEGER PRIMARY KEY, val INTEGER )";
+
+        dbcmd.CommandText = q_createTable;
+        reader = dbcmd.ExecuteReader();
+
+        // Insert values in table
+        IDbCommand cmnd = dbcon.CreateCommand();
+        cmnd.CommandText = "INSERT INTO my_table (id, val) VALUES (0, 5)";
+        cmnd.ExecuteNonQuery();
+
+        // Read and print all values in table
+        IDbCommand cmnd_read = dbcon.CreateCommand();
+        string query = "SELECT * FROM my_table";
+        cmnd_read.CommandText = query;
+        reader = cmnd_read.ExecuteReader();
+        while (reader.Read())
+        {
+            Debug.Log("id: " + reader[0].ToString());
+            Debug.Log("val: " + reader[1].ToString());
+        }
+
+        // Close connection
+        dbcon.Close();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
