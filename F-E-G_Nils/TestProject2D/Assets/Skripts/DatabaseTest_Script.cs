0f162239a6633052860262df010677670a64842d
NBoennigh <nils.boennighausen@rub.de>
2019-05-16 20:19:01 +0200

    Database fixed (finally)

---
+++ F-E-G_Nils/TestProject2D/Assets/Skripts/DatabaseTest_Script.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Data;
+//using Mono.Data.Sqlite;
+using System.IO;
+
+public class DatabaseTest_Script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        string connect = "url=file:" + Application.persistentDataPath + "/myDatabase";
+        //IDbConnection dbCon = new SqliteConnection(connect);
+        //dbCon.open();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
