c9894fe5e43bec5f83397868eddc5e73b455b9c5
Vyrtan <jonas_langenberg@web.de>
2019-05-16 20:07:51 +0200

    fixed db

---
+++ F-E-G_Jonas/Proto_Jonas/Assets/scripts/QuizFrage.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QuizFrage : MonoBehaviour {
+
+    int id;
+    string frage;
+    string[] antworten;
+    string richtigeAntwort;
+}
--- F-E-G_Jonas/Proto_Jonas/Assets/scripts/sqlliteTest.cs
+++ F-E-G_Jonas/Proto_Jonas/Assets/scripts/sqlliteTest.cs
@@ -13,19 +13,34 @@ public class sqlliteTest : MonoBehaviour {
         IDbConnection dbcon = new SqliteConnection(connection);
         dbcon.Open();
 
-        // Create table
+        // Drop old Table
         IDbCommand dbcmd;
         IDataReader reader;
+
         dbcmd = dbcon.CreateCommand();
-        string q_createTable = "CREATE TABLE IF NOT EXISTS my_table (id INTEGER PRIMARY KEY, val INTEGER )";
+        string q_dropTable = "DROP TABLE IF EXISTS my_table";
+
+        dbcmd.CommandText = q_dropTable;
+        reader = dbcmd.ExecuteReader();
+
+        //create new table
+        dbcmd = dbcon.CreateCommand();
+        string q_createTable = "CREATE TABLE IF NOT EXISTS my_table (id INTEGER PRIMARY KEY, frage STRING, antworten STRING, loesung STRING )";
 
         dbcmd.CommandText = q_createTable;
         reader = dbcmd.ExecuteReader();
 
         // Insert values in table
         IDbCommand cmnd = dbcon.CreateCommand();
-        cmnd.CommandText = "INSERT INTO my_table (id, val) VALUES (0, 5)";
+        cmnd.CommandText = "INSERT INTO my_table (id, frage, antworten, loesung) VALUES (0, 'Wer ist der Kaiser von China?', 'Ich | Du | Trump?', 'Trump?')";
         cmnd.ExecuteNonQuery();
+
+        cmnd.CommandText = "INSERT INTO my_table (id, frage, antworten, loesung) VALUES (1, 'Was ist Trumps leibgericht?', 'Burger! | Salat | Sushi', 'Burger!')";
+        cmnd.ExecuteNonQuery();
+
+        cmnd.CommandText = "INSERT INTO my_table (id, frage, antworten, loesung) VALUES (2, 'Du bist immernoch da?', 'Ja | Nein | Ups', 'Ups')";
+        cmnd.ExecuteNonQuery();
+
 
         // Read and print all values in table
         IDbCommand cmnd_read = dbcon.CreateCommand();
@@ -36,6 +51,8 @@ public class sqlliteTest : MonoBehaviour {
         {
             Debug.Log("id: " + reader[0].ToString());
             Debug.Log("val: " + reader[1].ToString());
+            Debug.Log("val: " + reader[2].ToString());
+            Debug.Log("val: " + reader[3].ToString());
         }
 
         // Close connection
