7b20566686a78cd46cf015e49646b33cf7119c07
NBoennigh <nils.boennighausen@rub.de>
2019-05-16 20:39:53 +0200

    Included Database Object in game, set up intial table

--- F-E-G_Nils/TestProject2D/Assets/Skripts/DatabaseTest_Script.cs
+++ F-E-G_Nils/TestProject2D/Assets/Skripts/DatabaseTest_Script.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Data;
-//using Mono.Data.Sqlite;
+using Mono.Data.Sqlite;
 using System.IO;
 
 public class DatabaseTest_Script : MonoBehaviour
@@ -10,9 +10,20 @@ public class DatabaseTest_Script : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        string connect = "url=file:" + Application.persistentDataPath + "/myDatabase";
-        //IDbConnection dbCon = new SqliteConnection(connect);
-        //dbCon.open();
+        string connect = "uri=file:" + Application.persistentDataPath + "/myDatabase";
+        IDbConnection dbcon = new SqliteConnection(connect);
+        dbcon.Open();
+
+        IDbCommand command;
+        IDataReader reader;
+
+        command = dbcon.CreateCommand();
+
+        string query = "CREATE TABLE IF NOT EXISTS table (id INTEGER PRIMARY KEY, question STRING, answers STRING)";
+
+        command.CommandText = query;
+        reader = command.ExecuteReader();
+
     }
 
     // Update is called once per frame
